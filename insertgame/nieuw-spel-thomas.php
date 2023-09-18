dbdb77da6136ebba5b3c57b34461d3a36179beda
Thomas Mooij <kootblad@live.nl>
2023-09-18 11:46:18 +0200

    nieuwspel verbeterd

--- insertgame/nieuw-spel-thomas.php
+++ insertgame/nieuw-spel-thomas.php
@@ -6,7 +6,7 @@ $password = "";
 
 $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 
-$query = "INSERT INTO games (titel, uitgever,platform, voorraad,prijs) 
+$query = "INSERT INTO producten (titel, uitgever,platform, voorraad,prijs) 
 VALUES('F1 2022' , 'EA Sports', 'Playstation 5', '28','69')";
 
 $conn->exec($query)
